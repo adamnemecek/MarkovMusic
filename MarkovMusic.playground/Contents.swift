4120f541127e4cb638c2b300eeb9a7189386f21c
idgd <isaac.dudney@icloud.com>
2016-11-10 20:50:18 -0800

    fixed up lots of code, added partially functioning note to midi translation

--- MarkovMusic.playground/Contents.swift
+++ MarkovMusic.playground/Contents.swift
@@ -15,21 +15,194 @@ struct Track {
     var name:String
     var notes:[Note]
     
+    init(name: String, notes: [Note]) {
+        self.name = name
+        self.notes = notes
+    }
+    
+    init(track: MusicTrack, named: String) {
+        
+        var iterator: MusicEventIterator?
+        NewMusicEventIterator(track, &iterator)
+        
+        var hasNext: DarwinBoolean = false
+        MusicEventIteratorHasCurrentEvent(iterator!, &hasNext)
+        
+        notes = []
+        name = named
+        
+        while hasNext.boolValue {
+            
+            var timestamp: MusicTimeStamp = 0
+            var eventType: MusicEventType = 0
+            var eventData: UnsafeRawPointer? = nil
+            var eventDataSize: UInt32 = 0
+            
+            MusicEventIteratorGetEventInfo(iterator!,
+                                           &timestamp,
+                                           &eventType,
+                                           &eventData,
+                                           &eventDataSize);
+            
+            if eventType == kMusicEventType_MIDINoteMessage {
+                
+                let message = eventData!.assumingMemoryBound(to: MIDINoteMessage.self)
+                //print(message[0])
+                let note = Note(note: message[0].note, velocity: message[0].velocity, duration: message[0].duration)
+                notes.append(note)
+                
+            }
+            
+            MusicEventIteratorNextEvent(iterator!)
+            MusicEventIteratorHasCurrentEvent(iterator!, &hasNext)
+            
+        }
+        
+    }
+    
+    func musicTrack(channel: UInt8, sequence: MusicSequence) -> MusicTrack? {
+        
+        var timing: MusicTimeStamp = 0.0
+        
+        var musicTrack: MusicTrack? = nil
+        
+        let status = MusicSequenceNewTrack(sequence, &musicTrack)
+        
+        if status == OSStatus(noErr) && musicTrack != nil {
+            for f in self.notes {
+                
+                var midiNote = MIDINoteMessage(channel: UInt8(channel), note: UInt8(f.note), velocity: UInt8(f.velocity), releaseVelocity: UInt8(0), duration: Float32(f.duration))
+                print(midiNote)
+                
+                print(timing)
+                
+                let newNoteStatus = MusicTrackNewMIDINoteEvent(musicTrack!, timing, &midiNote)
+                
+                timing += MusicTimeStamp(f.duration)
+                print(timing)
+                
+                if newNoteStatus != OSStatus(noErr) {
+                    print("failed to make a note")
+                } else {
+                    print("Made a note \(f.note)")
+                }
+
+            }
+        }
+        
+        return musicTrack
+        
+    }
+    
 }
 
 struct Note {
     
     var note:UInt8
     var velocity:UInt8
-    var duration:Float
+    var duration:Float32
     
 }
 
-struct MarkovNote {
+struct MarkovNoteChain {
     
     var noteDict:Dictionary<UInt8, [UInt8]>
     var velocityDict:Dictionary<UInt8, [UInt8]>
     var durationDict:Dictionary<Float, [Float]>
+    
+    init(track: Track) {
+        
+        noteDict = [UInt8: [UInt8]]()
+        velocityDict = [UInt8: [UInt8]]()
+        durationDict = [Float: [Float]]()
+        
+        for f in track.notes {
+            noteDict[f.note] = []
+            velocityDict[f.velocity] = []
+            durationDict[f.duration] = []
+        }
+        
+        var noteArr: [UInt8] = []
+        var velocityArr: [UInt8] = []
+        var durationArr: [Float] = []
+        
+        for (note, _) in noteDict {
+            for (noteIndex, noteItem) in track.notes.enumerated() {
+                if note == noteItem.note && noteIndex < track.notes.count - 2 {
+                    noteArr.append(track.notes[noteIndex + 1].note)
+                }
+            }
+            
+            noteDict[note] = noteArr
+            noteArr.removeAll(keepingCapacity: true)
+            
+        }
+        
+        for (velocity, _) in velocityDict {
+            for (velocityIndex, velocityItem) in track.notes.enumerated() {
+                if velocity == velocityItem.velocity && velocityIndex < track.notes.count - 2 {
+                    velocityArr.append(track.notes[velocityIndex + 1].velocity)
+                }
+            }
+            
+            velocityDict[velocity] = velocityArr
+            velocityArr.removeAll(keepingCapacity: true)
+            
+        }
+        
+        for (duration, _) in durationDict {
+            for (durationIndex, durationItem) in track.notes.enumerated() {
+                if duration == durationItem.duration && durationIndex < track.notes.count - 2 {
+                    durationArr.append(track.notes[durationIndex + 1].duration)
+                }
+            }
+            
+            durationDict[duration] = durationArr
+            durationArr.removeAll(keepingCapacity: true)
+            
+        }
+        
+    }
+
+    func nextNote(note: Note) -> Note {
+        
+        var noteNum: UInt8 = UInt8(arc4random_uniform(128))
+        var velocityNum: UInt8 = UInt8(arc4random_uniform(128))
+        var durationNum: Float = Float(arc4random_uniform(2))
+        
+        if let nextNotes = noteDict[note.note] {
+            noteNum = nextNotes[Int(arc4random_uniform(UInt32(nextNotes.count)))]
+        }
+        
+        if let nextVelocities = velocityDict[note.velocity] {
+            velocityNum = nextVelocities[Int(arc4random_uniform(UInt32(nextVelocities.count)))]
+        }
+        
+        if let nextDurations = durationDict[note.duration] {
+            let ind = Int(arc4random_uniform(UInt32(nextDurations.count)))
+            if nextDurations.count == 0 {
+                print("For some reason nextDurations is empty, defaulting to duration = 1")
+                durationNum = 1
+            } else {
+                durationNum = nextDurations[ind]
+            }
+        }
+        
+        return Note(note: noteNum, velocity: velocityNum, duration: durationNum)
+        
+    }
+    
+    func track(noteCount: Int, tempo: Float64, startNote: Note) -> Track {
+        var notes: [Note] = []
+        
+        var playNote = startNote
+        for _ in 0..<25 {
+            playNote = nextNote(note: playNote)
+            notes.append(playNote)
+        }
+        
+        return Track(name: "A", notes: notes)
+    }
     
 }
 
@@ -50,7 +223,7 @@ func createSong(url: URL) -> Song? {
         
         var trackCount:UInt32 = 0
         MusicSequenceGetTrackCount(sequence!, &trackCount)
-        
+
         var tracks: [Track] = []
         
         for i in 0..<trackCount {
@@ -58,7 +231,7 @@ func createSong(url: URL) -> Song? {
             var trackData: MusicTrack?
             MusicSequenceGetIndTrack(sequence!, i, &trackData)
             
-            let track = Track(name: "\(i)", notes: parseTrack(track: trackData!))
+            let track = Track(track: trackData!, named: "\(i)")
             
             tracks.append(track)
             
@@ -73,46 +246,6 @@ func createSong(url: URL) -> Song? {
         return nil
         
     }
-    
-}
-
-func parseTrack(track: MusicTrack) -> [Note] {
-    
-    var iterator: MusicEventIterator?
-    NewMusicEventIterator(track, &iterator)
-    
-    var hasNext: DarwinBoolean = false
-    MusicEventIteratorHasCurrentEvent(iterator!, &hasNext)
-    
-    var returnArray: [Note] = []
-    
-    while hasNext.boolValue {
-        
-        var timestamp: MusicTimeStamp = 0
-        var eventType: MusicEventType = 0
-        var eventData: UnsafeRawPointer? = nil
-        var eventDataSize: UInt32 = 0
-        
-        MusicEventIteratorGetEventInfo(iterator!,
-                                       &timestamp,
-                                       &eventType,
-                                       &eventData,
-                                       &eventDataSize);
-        
-        if eventType == kMusicEventType_MIDINoteMessage {
-            
-            let message = eventData!.assumingMemoryBound(to: MIDINoteMessage.self)
-            let note: Note = Note(note: message[0].note, velocity: message[0].velocity, duration: message[0].duration)
-            returnArray.append(note)
-            
-        }
-        
-        MusicEventIteratorNextEvent(iterator!)
-        MusicEventIteratorHasCurrentEvent(iterator!, &hasNext)
-        
-    }
-    
-    return returnArray
     
 }
 
@@ -142,138 +275,48 @@ func parseTempoTrack(track: MusicTrack) -> Float64? {
             let tempoData = eventData!.assumingMemoryBound(to: ExtendedTempoEvent.self)
             return tempoData[0].bpm
             
-        } else {
-            
-            return nil
         }
         
         MusicEventIteratorNextEvent(iterator!)
         MusicEventIteratorHasCurrentEvent(iterator!, &hasNext)
-        
     }
     
     return nil
 }
 
-func createMarkovNote(track: Track) -> MarkovNote {
-    
-    var noteDict = [UInt8: [UInt8]]()
-    var velocityDict = [UInt8: [UInt8]]()
-    var durationDict = [Float: [Float]]()
-    
-    for f in track.notes {
-        noteDict[f.note] = []
-        velocityDict[f.velocity] = []
-        durationDict[f.duration] = []
-    }
-    
-    var noteArr: [UInt8] = []
-    var velocityArr: [UInt8] = []
-    var durationArr: [Float] = []
-    
-    for (index, item) in noteDict.enumerated() {
-        for (noteIndex, noteItem) in track.notes.enumerated() {
-            if item.key == noteItem.note && noteIndex < track.notes.count - 1 {
-                noteArr.append(track.notes[noteIndex + 1].note)
-            }
-        }
-        
-        noteDict[item.key] = noteArr
-        noteArr = []
-        
-    }
-    
-    for (index, item) in velocityDict.enumerated() {
-        for (velocityIndex, velocityItem) in track.notes.enumerated() {
-            if item.key == velocityItem.velocity && velocityIndex < track.notes.count - 1 {
-                velocityArr.append(track.notes[velocityIndex + 1].velocity)
-            }
-        }
-        
-        velocityDict[item.key] = velocityArr
-        velocityArr = []
-        
-    }
-    
-    for (index, item) in durationDict.enumerated() {
-        for (durationIndex, durationItem) in track.notes.enumerated() {
-            if item.key == durationItem.duration && durationIndex < track.notes.count - 1 {
-                durationArr.append(track.notes[durationIndex + 1].duration)
-            }
-        }
-        
-        durationDict[item.key] = durationArr
-        durationArr = []
-        
-    }
-    
-    let returnMarkov = MarkovNote(noteDict: noteDict, velocityDict: velocityDict, durationDict: durationDict)
-    
-    return returnMarkov
-    
-}
-
-func nextNote(markov: MarkovNote, note: Note) -> Note {
-    
-    var noteNum: UInt8 = UInt8(arc4random_uniform(128))
-    var velocityNum: UInt8 = UInt8(arc4random_uniform(128))
-    var durationNum: Float = Float(arc4random_uniform(2))
-    
-    for f in markov.noteDict {
-        
-        if f.key == note.note {
-            
-            let randomIndex = Int(arc4random_uniform(UInt32(f.value.count)))
-            noteNum = f.value[randomIndex]
-            
-        }
-        
-    }
-    
-    for f in markov.velocityDict {
-        
-        if f.key == note.velocity {
-            
-            let randomIndex = Int(arc4random_uniform(UInt32(f.value.count)))
-            velocityNum = f.value[randomIndex]
-            
-        }
-        
-    }
-    
-    for f in markov.durationDict {
-        
-        if f.key == note.duration {
-            
-            let randomIndex = Int(arc4random_uniform(UInt32(f.value.count)))
-            durationNum = f.value[randomIndex]
-            
-        }
-        
-    }
-    
-    let returnNote: Note = Note(note: noteNum, velocity: velocityNum, duration: durationNum)
-    
-    print(returnNote)
-    
-    return returnNote
-    
-}
 
 let path = Bundle.main.path(forResource: "d_bunny", ofType: "mid")!
 let url = URL(fileURLWithPath: path)
 
+
 if let song = createSong(url: url) {
     
-    for f in song.tracks {
-        
-        for g in f.notes {
-            
-            let markovNoteChain = createMarkovNote(track: f)
-            nextNote(markov: markovNoteChain, note: g)
-            
-        }
-        
+    var sequence: MusicSequence?
+    let status = NewMusicSequence(&sequence)
+    
+    let markovNoteChain = MarkovNoteChain(track: song.tracks[0])
+
+    var playNote = song.tracks[0].notes[0]
+//    for i in 0..<25 {
+//        playNote = markovNoteChain.nextNote(note: playNote)
+//    }
+    
+    let newTrack = markovNoteChain.track(noteCount: 25, tempo: 120, startNote: playNote)
+    newTrack.musicTrack(channel: 0, sequence: sequence!)
+    
+    var trackCount:UInt32 = 0
+    MusicSequenceGetTrackCount(sequence!, &trackCount)
+    
+    print(trackCount)
+    
+//    public func NewMusicPlayer(_ outPlayer: UnsafeMutablePointer<MusicPlayer?>) -> OSStatus
+    
+    var player: MusicPlayer?
+    let playerStatus = NewMusicPlayer(&player)
+    
+    if playerStatus == OSStatus(noErr) && player != nil {
+        MusicPlayerSetSequence(player!, sequence!)
+        MusicPlayerStart(player!)
     }
     
 } else {
@@ -281,3 +324,4 @@ if let song = createSong(url: url) {
     print("No song")
     
 }
+
